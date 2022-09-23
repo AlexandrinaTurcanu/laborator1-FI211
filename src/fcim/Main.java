9e61e7d84797282e8277110349766bf554f7d451
Alexandrina <turcanualexandrina@gmail.com>
2022-09-23 20:18:57 +0300

    Finalizare laborator

--- src/fcim/Main.java
+++ src/fcim/Main.java
@@ -1,5 +1,16 @@
 package fcim;
 
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        book1.afisare();
+
+        Book book2 = new Book("Cartier","Ernest Hemingway", "Pentru cine bat clopotele", 2017, 378);
+        book2.afisare();
+
+        Book book3 = new Book("Adevarul","Vasili Grossman", "Viata si destin", 2015, 612);
+        book3.afisare();
+    }
+
 
 }
